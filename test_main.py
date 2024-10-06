6ed4f26034795f08fa5ecbd445b575bba42ecc94
ting <justin899950@gmail.com>
2024-10-06 21:51:29 +0800

    Debug version of main

--- test_main.py
+++ test_main.py
@@ -22,29 +22,29 @@ arrival_time = datetime.strptime(row['time_abs(%Y-%m-%dT%H:%M:%S.%f)'], '%Y-%m-%
 arrival_time_rel = row['time_rel(sec)']
 test_filename = row.filename
 
-## 匯入 spectrogram 的 csv
+########################################### 匯入 spectrogram 的 csv ###########################################
 spec_input = './space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA/'      # Input miniseed
 spec_directory = './output/'                                                                      # Output csv
-spectrogram_detect(spec_input, spec_directory)
+# spectrogram_detect(spec_input, spec_directory)    # 暫時不跑演算法，只記資料
 spec_file = spec_directory + 'spectrogram_output.csv'
 spec = pd.read_csv(spec_file)
 # 從目錄中取得到達時間
 spec_row = spec.iloc[r]
-# print(spec_row)
+print(spec_row)
 spec_arrival_time = datetime.strptime(spec_row['time_abs(%Y-%m-%dT%H:%M:%S.%f)'], '%Y-%m-%dT%H:%M:%S.%f')
 # 取得相對到達時間及檔案名稱
 spec_arrival_time_rel = spec_row['time_rel(sec)']
 spec_test_filename = spec_row.filename
 
-## 匯入 LOF 的 csv
+########################################### 匯入 LOF 的 csv ###########################################
 LOF_input = './space_apps_2024_seismic_detection/data/lunar/training/data/S12_GradeA/'           # Input csv
 LOF_directory = './output/'                                                                      # Output csv
-# LOF_detect(LOF_input, LOF_directory)
+# LOF_detect(LOF_input, LOF_directory)    # 暫時不跑演算法，只記資料
 LOF_file = LOF_directory + 'LOF_output.csv'
 LOF = pd.read_csv(LOF_file)
 # 從目錄中取得到達時間
 LOF_row = LOF.iloc[r]
-# print(LOF_row)
+print(LOF_row)
 LOF_arrival_time = datetime.strptime(LOF_row['time_abs(%Y-%m-%dT%H:%M:%S.%f)'], '%Y-%m-%dT%H:%M:%S.%f')
 # 取得相對到達時間及檔案名稱
 LOF_arrival_time_rel = LOF_row['time_rel(sec)']
@@ -90,7 +90,6 @@ fig = plt.figure(figsize=(10, 10))
 ax = plt.subplot(2, 1, 1)
 ax.plot(tr_times_filt, tr_data_filt)
 ax.axvline(x=arrival, color='red', label='Detection')
-# ax.legend(loc='upper left')
 ax.axvline(x=spec_arrival,c='green', label=f'Spactrogram_Estimated', linestyle='--')
 ax.axvline(x=LOF_arrival,c='yellow', label=f'LOF_Estimated', linestyle='--')
 ax.legend(loc='upper left')
@@ -104,7 +103,7 @@ ax2.set_xlim([min(tr_times_filt), max(tr_times_filt)])
 ax2.set_xlabel(f'Time (Day Hour:Minute)', fontweight='bold')
 ax2.set_ylabel('Frequency (Hz)', fontweight='bold')
 ax.set_title(f'{test_filename}', fontweight='bold')
-# # 標記檢測到的事件和估算事件（多個事件）
+# plot estimated event
 ax2.axvline(x=arrival, c='red', label='Catalog Detection')
 ax2.axvline(x=spec_arrival,c='green', label=f'Spactrogram_Estimated', linestyle='--')
 ax2.axvline(x=LOF_arrival,c='yellow', label=f'LOF_Estimated', linestyle='--')
@@ -113,55 +112,6 @@ cbar = plt.colorbar(vals, orientation='horizontal')
 cbar.set_label('Power ((m/s)^2/sqrt(Hz))', fontweight='bold')
 
 ax2.legend(loc='upper left')
-
-# # STA/LTA 參數
-# from obspy.signal.trigger import classic_sta_lta, trigger_onset
-
-# df = tr.stats.sampling_rate
-# sta_len = 120
-# lta_len = 600
-
-# # 使用 STA/LTA 計算特徵函數
-# cft = classic_sta_lta(tr_data, int(sta_len * df), int(lta_len * df))
-
-# # 繪製特徵函數
-# fig, ax = plt.subplots(1, 1, figsize=(12, 3))
-# ax.plot(tr_times, cft)
-# ax.set_xlim([min(tr_times), max(tr_times)])
-# ax.set_xlabel('Time (s)')
-# ax.set_ylabel('Characteristic function')
-
-# # 設定觸發閾值
-# thr_on = 4
-# thr_off = 1.5
-# on_off = np.array(trigger_onset(cft, thr_on, thr_off))
-
-# # 繪製觸發點
-# colors = plt.get_cmap('tab10', len(on_off))
-# fig, ax = plt.subplots(1, 1, figsize=(12, 3))
-
-# for i in range(len(on_off)):
-#     triggers = on_off[i]
-#     color = colors(i % 10)
-#     ax.axvline(x=tr_times[triggers[0]], color=color, label=f'Trig. On {i}')
-#     ax.axvline(x=tr_times[triggers[1]], color=color, linestyle='--', label=f'Trig. Off {i}')
-
-# ax.plot(tr_times, tr_data)
-# ax.set_xlim([min(tr_times), max(tr_times)])
-# ax.legend()
-
-# # 將檢測時間整理到一個 dataframe
-# detection_times = []
-# fnames = []
-# event_flags = []
-
-# for i in range(len(on_off)):
-#     triggers = on_off[i]
-#     on_time = starttime + timedelta(seconds=tr_times[triggers[0]])
-#     on_time_str = datetime.strftime(on_time, '%Y-%m-%dT%H:%M:%S.%f')
-#     detection_times.append(on_time_str)
-#     fnames.append(test_filename)
-#     event_flags.append(1)
 
 # 整理檢測結果成 dataframe
 # detect_df = pd.DataFrame(data={'filename': fnames, 'time_abs(%Y-%m-%dT%H:%M:%S.%f)': detection_times, 'time_rel(sec)': [tr_times[on_off[i][0]] for i in range(len(on_off))], 'event': event_flags})
